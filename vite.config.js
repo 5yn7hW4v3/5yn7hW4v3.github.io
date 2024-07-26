06a20f6f93b7077d61d7aab18d980646b1a4b6b6
5n7hW4v3 <73319380+5yn7hW4v3@users.noreply.github.com>
2024-07-25 19:35:08 -0500

    remove base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/5yn7hW4v3.github.io/"
+  //base: "/5yn7hW4v3.github.io/"
 })
